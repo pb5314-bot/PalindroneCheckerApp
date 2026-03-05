419396c75db6a872f29aef3e56c794361da584fc
PRANAV RAM <pb5314@srmist.edu.in>
2026-03-05 08:41:41 +0530

    Added UC9 Recursive Palindrome Checker

---
+++ app/src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive function to check palindrome
+    public static boolean isPalindrome(String word, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // Compare characters
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(word, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        // Call recursive function
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+
+        // Print result
+        if (result) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
