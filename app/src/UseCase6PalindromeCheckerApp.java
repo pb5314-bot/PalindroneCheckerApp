724be8c811fadcdbd0aacb9fd39275c953c9b313
PRANAV RAM <pb5314@srmist.edu.in>
2026-03-05 08:33:06 +0530

    Added UC6 Queue and Stack Based Palindrome Checker

---
+++ app/src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "radar";
+
+        // Create Stack and Queue
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push into stack and enqueue into queue
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue (queue) and pop (stack)
+        while (!stack.isEmpty()) {
+
+            char stackChar = stack.pop();
+            char queueChar = queue.remove();
+
+            if (stackChar != queueChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
