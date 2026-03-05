631b41184d577894c89d221ec4d82682c6e5153b
PRANAV RAM <pb5314@srmist.edu.in>
2026-03-05 08:24:08 +0530

    Added UC3 Palindrome Checker

---
+++ app/src/UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String original = "racecar";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is not a Palindrome");
+        }
+    }
+}
